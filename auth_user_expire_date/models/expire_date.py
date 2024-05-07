a475b030995e928644c1945b558ad8aa843d09ae
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-05-07 14:50:46 +0200

    Update expire_date.py

--- auth_user_expire_date/models/expire_date.py
+++ auth_user_expire_date/models/expire_date.py
@@ -1,1 +1,7 @@
-## expire date
+from odoo import models, fields, api, _
+
+
+class Project(models.Model):
+    _inherit = "res.users"
+
+    date_expire = fields.Date('res.users', string="Date expire")
